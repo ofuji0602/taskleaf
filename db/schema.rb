7888fb1e76b9dfb70055585ef4d3d6b73dd6f347
ofuji0602 <hitujimofumofu0206@gmail.com>
2023-12-27 07:27:04 +0900

    Update .gitignore and db/schema.rb for Chapter-5

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_12_24_170136) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
